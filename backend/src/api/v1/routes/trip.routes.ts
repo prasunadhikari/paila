3c65ed06c563d3a5c051886fe04e99a9b3d75887
Prasun Adhikari <aprasun44@gmail.com>
2026-07-12 09:45:39 +0530

    Fix ESM import

--- backend/src/api/v1/routes/trip.routes.ts
+++ backend/src/api/v1/routes/trip.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { generateTripController } from "../controllers/trip.controller";
+import { generateTripController } from "../controllers/trip.controller.js";
 
 const router = Router();
 
