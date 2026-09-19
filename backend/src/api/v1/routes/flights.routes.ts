9926b949f8b25b6b384b83fe38e57ab142cac1fc
Prasun Adhikari <bprasun44@gmail.com>
2026-09-19 14:56:53 +0545

    Fix flight controller import

--- backend/src/api/v1/routes/flights.routes.ts
+++ backend/src/api/v1/routes/flights.routes.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import {
   searchFlights,
   getFlightBookingLinks,
-} from "../controllers/flights.controller";
+} from "../controllers/flights.controller.js";
 
 const router = Router();
 
